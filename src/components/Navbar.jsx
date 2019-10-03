6b34719589b5f3575b8e73cb5a4400dc7ae8cd21
zee <zzdd9251@gmail.com>
2019-10-02 19:43:28 -0700

    intal..

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,44 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+// import { useDarkMode } from '../Hook/useDarkMode';
+
+// const Navbar = () => {
+//   const [darkMode, setDarkMode] = useDarkMode(false);
+//   const toggleMode = e => {
+//     e.preventDefault();
+//     setDarkMode(!darkMode);
+//   };
+//   return (
+//     <nav className="navbar">
+//       <h1>Crypto Tracker</h1>
+//       {/* <NavLink>home</NavLink> */}
+//       <div className="dark-mode__toggle">
+//         <div
+//           onClick={toggleMode}
+//           className={darkMode ? 'toggle toggled' : 'toggle'}
+//         />
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+
+
+import React from 'react';
+import { useDarkMode } from '../Hook/useDarkMode';
+
 
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useDarkMode(false);
   const toggleMode = e => {
     e.preventDefault();
     setDarkMode(!darkMode);
   };
   return (
-    <nav className="navbar">
+    <nav className='navbar'>
       <h1>Crypto Tracker</h1>
-      <div className="dark-mode__toggle">
+
+      <div className='dark-mode__toggle'>
         <div
           onClick={toggleMode}
           className={darkMode ? 'toggle toggled' : 'toggle'}
